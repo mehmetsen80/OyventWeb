d0e3e14a4462246dab8fa240d4eb43635330e078
Mehmet Sen <mehmetsen80@gmail.com>
2016-03-24 14:33:48 -0500

    update

--- login/index.php
+++ login/index.php
@@ -87,7 +87,7 @@ else if($region == "NY")
                     	<h2><b><u>Albums for your Community</u></b></h2>
                         <h3 style='font-size:22px;font-weight:normal;'>As photos flow social media, we help you to store your photos in one place. With Oyvent, you can view, import and share trending community photos.
 </h3><br>
-                        <h2><b><u>Enhance your experience at your community <?php //echo $album; ?></u></b></h2>
+                        <h2><b><u>Album is social <?php //echo $album; ?></u></b></h2>
                         <h3 style='font-size:22px;font-weight:normal;'>Oyvent allows you to add and view posts based on interests, and then group them together in a specific community.</h3>
                         <a href="/Explore.php" class="btn btn-default btn-lg">Explore</a>
                     </div>
